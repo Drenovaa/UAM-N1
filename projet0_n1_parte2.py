659de795f2c79f33bbf87903bf8955bf6ddd940c
Brunodias37 <62164559+Brunodias37@users.noreply.github.com>
2021-10-09 16:40:15 -0300

    Update_1
    Adição de um menu intermediário entre as escolhas

--- projet0_n1_parte2.py
+++ projet0_n1_parte2.py
@@ -1,9 +1,21 @@
-
+import time 
 global nomeCompleto
 global eMail
 
-nomeCompleto = ['Tulio felipe', 'Joao da silva','Maria filomena','Carlos da Silva','Sandra souza']
+nomeCompleto = ['Tulio felipe', 'João da silva','Maria filomena','Carlos da Silva','Sandra souza']
 eMail=['tulio.felipe@hotmail.com','joao.silva@gmail.com', 'mariafilo@globo.com', 'carlos.silva@amazon','sansou@netflix.com.br']
+
+def retornoMenuInicial():
+    print("[1]--Retornar ao Menu Inicial")
+    print("[2]--Sair")
+    resposta = int(input())
+    if resposta == 1:
+        menu()
+    elif resposta == 2:
+        exit(0)
+    else:
+        print("Resposta invalida")
+        return resposta
 
 
 def incluiNome ():
